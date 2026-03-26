a962a5f08e9d886f9c3c93f1e3840305daebe83a
kai <taidt3004@gmail.com>
2026-03-26 08:51:47 +0700

    Initial Commit

---
+++ DataShuttle/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DataShuttle
+//
+//  Created by tai on 26/3/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DataShuttleTests/DataShuttleTests.swift
@@ -0,0 +1,19 @@
+//
+//  DataShuttleTests.swift
+//  DataShuttleTests
+//
+//  Created by tai on 26/3/26.
+//
+
+import Testing
+@testable import DataShuttle
+
+struct DataShuttleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
